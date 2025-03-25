f2a433e6e6f7058d0d6adb608d5525b16598d3ae
MaximSizyakin <maxim.sizyakin@gmail.com>
2025-03-25 17:45:23 +0300

    second commit

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <AppHeader/>
+    <slot/>
+    <AppFooter/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "default"
+};
+</script>
+
+<style scoped>
+
+</style>
